b3fce68dad97e72dcf9f9e37ea11b538f60be539
Chris Wolf <chriswolfdesign@gmail.com>
2025-08-09 16:49:46 -0400

    Adds temporary exercise to system (#2)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import ExerciseViewer from "./components/exercise_viewer/ExerciseViewer";
 import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ExerciseViewer />
     </div>
   );
 }
---
+++ src/components/exercise_viewer/ExerciseViewer.jsx
@@ -0,0 +1,32 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardHeader from "@mui/material/CardHeader";
+
+const CARD_CONTENT_STYLE = {
+  textAlign: "center",
+  height: "75vh",
+};
+
+const CARD_STYLE = {
+  height: "75vh",
+};
+
+function ExerciseViewer() {
+  return (
+    <Card sx={CARD_STYLE}>
+      <CardHeader>Exercise time</CardHeader>
+      <CardContent sx={CARD_CONTENT_STYLE}>
+        <object
+          title="Exercise viewer"
+          width="80%"
+          height="100%"
+          data="exercises/example/example.pdf"
+        >
+          Exercise
+        </object>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default ExerciseViewer;
