79ea46ad3e6f1d96d0d368925a19d4322519f0c3
chriswolf <chriswolfdesign@gmail.com>
2025-08-10 22:17:35 -0400

    Adds triplet timing exercise

---
+++ src/components/exercise_dropdown/ExerciseDropdown.jsx
@@ -0,0 +1,45 @@
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import NativeSelect from "@mui/material/NativeSelect";
+
+const NATIVE_SELECT_STYLE = {
+  textAlign: "center",
+  width: "100%",
+};
+
+const DROPDOWN_STYLE = {
+  paddingLeft: "200px",
+};
+
+function ExerciseDropdown(props) {
+  function exerciseChange(e) {
+    props.updateExercise(e.target.value);
+  }
+
+  return (
+    <FormControl sx={DROPDOWN_STYLE}>
+      <InputLabel
+        sx={DROPDOWN_STYLE}
+        variant="standard"
+        htmlFor="uncontrolled-native"
+      >
+        Exercise
+      </InputLabel>
+      <NativeSelect
+        defaultValue="eights"
+        inputProps={{
+          name: "exercise",
+          id: "uncontrolled-native",
+        }}
+        sx={NATIVE_SELECT_STYLE}
+        onChange={exerciseChange}
+      >
+        <option value="eights">Eights</option>
+        <option value="sixteenth_note_timing">Sixteenth Note Timing</option>
+        <option value="triplet_timing">Triplet Timing</option>
+      </NativeSelect>
+    </FormControl>
+  );
+}
+
+export default ExerciseDropdown;
--- src/components/exercise_manager/ExerciseManager.jsx
+++ src/components/exercise_manager/ExerciseManager.jsx
@@ -3,28 +3,48 @@ import ExercisePlayer from "../exercise_player/ExercisePlayer";
 import ExerciseViewer from "../exercise_viewer/ExerciseViewer";
 import PdfDropdown from "../pdf_dropdown/PdfDropdown";
 import TempoDropdown from "../tempo_dropdown/TempoDropdown";
+import ExerciseDropdown from "../exercise_dropdown/ExerciseDropdown";
 
 const DEFAULT_PDF = "exercises/eights/pdfs/eights_snare.pdf";
 const DEFAULT_MP3 = "exercises/eights/mp3s/eights_100.wav";
+const DEFAULT_TEMPO = "100";
+const DEFAULT_INSTRUMENT = "snare";
+const DEFAULT_EXERCISE = "eights";
 
 function ExerciseManager() {
   const [currentPDF, setCurrentPDF] = useState(DEFAULT_PDF);
   const [currentMP3, setCurrentMP3] = useState(DEFAULT_MP3);
+  const [currentTempo, setCurrentTempo] = useState(DEFAULT_TEMPO);
+  const [currentInstrument, setCurrentInstrument] =
+    useState(DEFAULT_INSTRUMENT);
+  const [currentExercise, setCurrentExercise] = useState(DEFAULT_EXERCISE);
 
   function updateInstrument(instrument) {
-    setCurrentPDF(`exercises/eights/pdfs/eights_${instrument}.pdf`);
+    setCurrentInstrument(instrument);
+    setCurrentPDF(
+      `exercises/${currentExercise}/pdfs/${currentExercise}_${instrument}.pdf`
+    );
   }
 
   function updateTempo(tempo) {
-    setCurrentMP3(`exercises/eights/mp3s/eights_${tempo}.wav`);
+    setCurrentTempo(tempo);
+    setCurrentMP3(
+      `exercises/${currentExercise}/mp3s/${currentExercise}_${tempo}.wav`
+    );
+  }
+
+  function updateExercise(exercise) {
+    setCurrentExercise(exercise);
+    setCurrentPDF(
+      `exercises/${exercise}/pdfs/${exercise}_${currentInstrument}.pdf`
+    );
+    setCurrentMP3(`exercises/${exercise}/mp3s/${exercise}_${currentTempo}.wav`);
   }
 
   return (
     <>
-      <PdfDropdown
-        setCurrentPDF={setCurrentPDF}
-        updateInstrument={updateInstrument}
-      />
+      <ExerciseDropdown updateExercise={updateExercise} />
+      <PdfDropdown updateInstrument={updateInstrument} />
       <TempoDropdown updateTempo={updateTempo} />
       <ExerciseViewer pdfLink={currentPDF} />
       <ExercisePlayer mp3Link={currentMP3} />
